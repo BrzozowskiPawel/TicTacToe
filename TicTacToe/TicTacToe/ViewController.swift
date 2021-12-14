b1e0e1dd08bec8f54c6cd1966dc02b59c6163eee
Paweł Brzozowski <mac@192.168.1.6>
2021-12-14 22:41:41 +0100

    added button for first screen

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,29 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var backgroundGradientView: UIView!
+    @IBOutlet weak var singleplayerButton: UIButton!
+    @IBOutlet weak var multiplayerButton: UIButton!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        
-        setBacgroundGradient()
-        
     }
 
-    func setBacgroundGradient() {
-        // Create a gradient layer.
-        let gradientLayer = CAGradientLayer()
-        // Set the size of the layer to be equal to size of the display.
-        gradientLayer.frame = view.bounds
-        // Set an array of Core Graphics colors (.cgColor) to create the gradient.
-        let firstColor = UIColor(red: 0/255, green: 210/255, blue: 255/255, alpha: 1).cgColor
-        let secondColor = UIColor(red: 58/255, green: 123/255, blue: 213/255, alpha: 1).cgColor
-        gradientLayer.colors = [ firstColor, secondColor]
-        // Rasterize this static layer to improve app performance.
-        gradientLayer.shouldRasterize = true
-        // Apply the gradient to the backgroundGradientView.
-        backgroundGradientView.layer.addSublayer(gradientLayer)
-    }
 }
 
