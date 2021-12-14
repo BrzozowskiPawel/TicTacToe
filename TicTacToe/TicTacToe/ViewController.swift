64654b310001e903e7f2195235dac22f032f85ec
Paweł Brzozowski <mac@192.168.1.6>
2021-12-14 21:25:56 +0100

    initial commit

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Paweł Brzozowski on 14/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
