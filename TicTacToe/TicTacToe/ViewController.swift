66343b4fb8c935d7eb0633e1b7ba2086a007e5d7
Paweł Brzozowski <mac@192.168.1.6>
2021-12-15 14:58:58 +0100

    added LanuchScreen

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,9 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var singleplayerButton: UIButton!
-    @IBOutlet weak var multiplayerButton: UIButton!
-    
     override func viewDidLoad() {
         
         super.viewDidLoad()
