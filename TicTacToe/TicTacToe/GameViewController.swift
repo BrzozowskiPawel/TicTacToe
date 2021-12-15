ca48f83d22bdea42defe58b814bbb5129eecfa9d
Paweł Brzozowski <mac@192.168.1.6>
2021-12-15 20:22:45 +0100

    setting tiles is working

--- TicTacToe/TicTacToe/GameViewController.swift
+++ TicTacToe/TicTacToe/GameViewController.swift
@@ -23,8 +23,19 @@ class GameViewController: UIViewController {
     @IBOutlet weak var gameView: UIView!
     
     
+    var playerType: String?
+    var occupiedTiles = [
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: "",
+        6: "",
+        7: "",
+        8: "",
+        9: "",
+    ]
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,29 +43,39 @@ class GameViewController: UIViewController {
         configureScreen()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     func configureScreen() {
         // Setup the buttons FIRST ROW
-        buttonA1.setImage(UIImage(named: "X_SPACE.png"), for: .normal)
+        buttonA1.setImage(UIImage(named: "DEFAULT_SPACE.png"), for: .normal)
         buttonA1.setTitle("", for: .normal)
         buttonA2.setImage(UIImage(named: "DEFAULT_SPACE.png"), for: .normal)
         buttonA2.setTitle("", for: .normal)
-        buttonA3.setImage(UIImage(named: "O_SPACE.png"), for: .normal)
+        buttonA3.setImage(UIImage(named: "DEFAULT_SPACE.png"), for: .normal)
         buttonA3.setTitle("", for: .normal)
-        
+
         // Setup the buttons SECOND ROW
-        buttonB1.setImage(UIImage(named: "X_SPACE.png"), for: .normal)
+        buttonB1.setImage(UIImage(named: "DEFAULT_SPACE.png"), for: .normal)
         buttonB1.setTitle("", for: .normal)
         buttonB2.setImage(UIImage(named: "DEFAULT_SPACE.png"), for: .normal)
         buttonB2.setTitle("", for: .normal)
-        buttonB3.setImage(UIImage(named: "O_SPACE.png"), for: .normal)
+        buttonB3.setImage(UIImage(named: "DEFAULT_SPACE.png"), for: .normal)
         buttonB3.setTitle("", for: .normal)
         
         // Setup the buttons THIRD ROW
-        buttonC1.setImage(UIImage(named: "X_SPACE.png"), for: .normal)
+        buttonC1.setImage(UIImage(named: "DEFAULT_SPACE.png"), for: .normal)
         buttonC1.setTitle("", for: .normal)
         buttonC2.setImage(UIImage(named: "DEFAULT_SPACE.png"), for: .normal)
         buttonC2.setTitle("", for: .normal)
-        buttonC3.setImage(UIImage(named: "O_SPACE.png"), for: .normal)
+        buttonC3.setImage(UIImage(named: "DEFAULT_SPACE.png"), for: .normal)
         buttonC3.setTitle("", for: .normal)
         
         // Set corner radius for game
@@ -65,5 +86,72 @@ class GameViewController: UIViewController {
         gameView.layer.cornerRadius = 15
     }
     
-
+    // MARK: - BUTTON PRESSED
+    
+    func TileIsFree(tileId: Int) -> Bool{
+        if occupiedTiles[tileId] == "" {
+            occupiedTiles[tileId] = self.playerType!
+            return true
+        } else {
+            print("Sorry already occupied by: \(occupiedTiles[tileId]!)")
+            return false
+        }
+    }
+    
+    
+    @IBAction func buttonA1Pressed(_ sender: Any) {
+        if TileIsFree(tileId: 1) {
+            buttonA1.setImage(UIImage(named: "\(self.playerType!)_SPACE.png"), for: .normal)
+        }
+        
+    }
+    
+    @IBAction func buttonA2Pressed(_ sender: Any) {
+        if TileIsFree(tileId: 2) {
+            buttonA2.setImage(UIImage(named: "\(self.playerType!)_SPACE.png"), for: .normal)
+        }
+    }
+    
+    @IBAction func buttonA3Pressed(_ sender: Any) {
+        if TileIsFree(tileId: 3) {
+            buttonA3.setImage(UIImage(named: "\(self.playerType!)_SPACE.png"), for: .normal)
+        }
+    }
+    
+    @IBAction func buttonB1Pressed(_ sender: Any) {
+        if TileIsFree(tileId: 4) {
+            buttonB1.setImage(UIImage(named: "\(self.playerType!)_SPACE.png"), for: .normal)
+        }
+    }
+    
+    @IBAction func buttonB2Pressed(_ sender: Any) {
+        if TileIsFree(tileId: 5) {
+            buttonB2.setImage(UIImage(named: "\(self.playerType!)_SPACE.png"), for: .normal)
+        }
+    }
+    
+    @IBAction func buttonB3Pressed(_ sender: Any) {
+        if TileIsFree(tileId: 6) {
+            buttonB3.setImage(UIImage(named: "\(self.playerType!)_SPACE.png"), for: .normal)
+        }
+    }
+    
+    @IBAction func buttonC1Pressed(_ sender: Any) {
+        if TileIsFree(tileId: 7) {
+            buttonC1.setImage(UIImage(named: "\(self.playerType!)_SPACE.png"), for: .normal)
+        }
+    }
+    @IBAction func buttonC2Pressed(_ sender: Any) {
+        if TileIsFree(tileId: 8) {
+            buttonC2.setImage(UIImage(named: "\(self.playerType!)_SPACE.png"), for: .normal)
+        }
+    }
+    
+    @IBAction func buttonC3Pressed(_ sender: Any) {
+        if TileIsFree(tileId: 9) {
+            buttonC3.setImage(UIImage(named: "\(self.playerType!)_SPACE.png"), for: .normal)
+        }
+    }
+    
+    
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var xButton: UIButton!
     @IBOutlet weak var oButton: UIButton!
     
+    var choosenType: String?
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -21,12 +23,29 @@ class ViewController: UIViewController {
         xButton.setTitle("", for: .normal)
         oButton.setImage(UIImage(named: "o_button.png"), for: .normal)
         oButton.setTitle("", for: .normal)
+        
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let gameVC = segue.destination as? GameViewController {
+            gameVC.playerType = self.choosenType
+        }
+    }
+    
     @IBAction func xButtonPressed(_ sender: UIButton) {
         print("x button pressed")
+        self.choosenType = "X"
+        goToGameVC()
     }
+    
     @IBAction func oButtonPressed(_ sender: UIButton) {
         print("o button pressed")
+        self.choosenType = "O"
+        goToGameVC()
+    }
+    
+    func goToGameVC() {
+        performSegue(withIdentifier: "gameSegue", sender: nil)
     }
     
 }
