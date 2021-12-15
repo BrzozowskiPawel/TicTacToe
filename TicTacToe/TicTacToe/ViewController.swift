5b37a101f0e63c8c9866b4d5b40793605c24fbb4
Paweł Brzozowski <mac@192.168.1.6>
2021-12-15 15:48:50 +0100

    fixed buttons error

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var xButton: UIButton!
+    @IBOutlet weak var oButton: UIButton!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        // Setup the buttons
+        xButton.setImage(UIImage(named: "x_button.png"), for: .normal)
+        xButton.setTitle("", for: .normal)
+        oButton.setImage(UIImage(named: "o_button.png"), for: .normal)
+        oButton.setTitle("", for: .normal)
     }
-
+    @IBAction func xButtonPressed(_ sender: UIButton) {
+        print("x button pressed")
+    }
+    @IBAction func oButtonPressed(_ sender: UIButton) {
+        print("o button pressed")
+    }
+    
 }
 
